6e0b1d26632cf7eb062a23e92bdbb892f2f4fc38
Lakshmi1301 <lakshmichokalingam13@gmail.com>
2020-03-10 21:52:44 +0530

    done with login page

--- src/test/java/org/airlines/ProjectAirlines.java
+++ src/test/java/org/airlines/ProjectAirlines.java
@@ -3,7 +3,9 @@ package org.airlines;
 public class ProjectAirlines {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hai");
+		
+		System.out.println("welcome");
 
 	}
 
